2490b1dda928be1e6a2b26bf48e2ce08fe2fc70b
Anton <roundronin8@gmail.com>
2024-09-30 07:42:21 +0300

    # Created Vehicle abstract classes
    Abstract class Vehicle defines what all types of transportation have
    - Name
    - Speed
    - CoordinateX that describse a position of vehicle in space relative to the origin (position of creation)
    - CalculatePosition() -- abstract method to calculate position
    - UpdateParams() -- abstract method to change desired parameters with time/distance
    
    The definitions of the abstract classes GroundTransport and AirTransport have specifics for their respective types of transport as well as the specific logic for recalculating the position for 1 game cycle, that is different for each of them.

---
+++ RaceSimulator/Transport/AbstractTransport.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceSimulator.Transport;
+
+public abstract class Vehicle
+{
+    protected double CoordinateX;
+
+    public string Name { get; set; }
+    public double Speed { get; set; }
+    public abstract double CalculatePosition();
+
+    protected abstract void UpdateParams();
+}
+
+public abstract class GroundTransport : Vehicle
+{
+    protected double timeWithoutRest;
+    protected double timeSpentResting;
+    protected bool resting = false;
+
+
+    public double TravelTimeBeforeRest { get; set; }
+    public double RestDuration { get; set; }
+    public double RestDurationCoefficient { get; set; }
+    public override double CalculatePosition()
+    {
+
+        if (resting && timeSpentResting <= RestDuration)
+        {
+            timeSpentResting++;
+            return CoordinateX;
+        }
+        else if (resting && timeSpentResting > RestDuration)
+        {
+            resting = false;
+            timeSpentResting = 0;
+        }
+
+        double distanceIncrement = Speed;
+        CoordinateX += distanceIncrement;
+
+        timeWithoutRest++;
+
+        if (timeWithoutRest >= TravelTimeBeforeRest)
+        {
+            resting = true;
+            timeWithoutRest = 0;
+        }
+
+        UpdateParams();
+
+        return CoordinateX;
+    }
+}
+
+public abstract class AirTransport : Vehicle
+{
+    public double AccelerationCoefficient { get; set; }
+
+    public override double CalculatePosition()
+    {
+        double distanceIncrement = Speed;
+        CoordinateX += distanceIncrement;
+
+        Speed += AccelerationCoefficient;
+
+        UpdateParams();
+
+        return CoordinateX;
+    }
+}
