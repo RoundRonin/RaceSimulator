e3eaddbc190d8d6d53dcccaf6bc0f02566b91816
Anton <roundronin8@gmail.com>
2024-10-29 20:44:39 +0300

    Added Interfaces for the Presentation layer
    Each interface serves it's own perpose:
    1. IPrinter -- an interface for printing structured messaged somewhere
    2. IReciever -- an interface to get specific business-logic-related information from the user
    3. IInformer -- an interface to print specific business-logic-related information somewhere

---
+++ RaceSimulator/Presentation/IReciever.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Race.Presentiation;
+
+internal interface IReciever
+{
+    double GetRaceDistance();
+    int GetRaceType();
+}
