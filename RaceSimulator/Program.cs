1cb917807a3b3e66a12f12fbfa60369b867ee02e
Anton <roundronin8@gmail.com>
2024-10-29 21:12:05 +0300

    Initialized the Factory
    Initial factory implementation, just to make things work as intended
    (follow SOLID)
    
    Factory creates vehicles based on the vehicle type number (subject to
    change)

--- RaceSimulator/Program.cs
+++ RaceSimulator/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using RaceSimulator.Presentation.Interfaces;
+using RaceSimulator.Presentation.CLI;
+using RaceSimulator.RaceLogic;
+using RaceSimulator.Transportation;
+
+
+IPrinter printer = new CommandLinePrinter();
+
+CLI cli = new CLI(printer);
+IReciever recieverCLI = cli as IReciever;
+IInformer informerCLI = cli as IInformer;
+
+// Cast check, just in case. For the future
+if (recieverCLI == null || informerCLI == null)
+{
+    throw new InvalidOperationException("CLI instance does not implement the required interfaces");
+}
+
+
+printer.PrintFormattedLine("Welcome", "Welcome to the Racing Simulator!");
+
+double distance = recieverCLI.GetRaceDistance();
+int raceType = recieverCLI.GetRaceType();
+
+RaceLogic race = new(distance, printer);
+
+List<string> vehicleTypes = raceType switch
+{
+    1 => new List<string> { "Centaur", "SevenLeagueBoots" },
+    2 => new List<string> { "MagicCarpet", "BabaYagasStupa" },
+    3 => new List<string> { "Centaur", "SevenLeagueBoots", "MagicCarpet", "BabaYagasStupa" },
+    _ => throw new ArgumentException("Invalid race type")
+};
+
+foreach (var vehicleType in vehicleTypes)
+{
+    race.RegisterVehicle(VehicleFactory.CreateVehicle(vehicleType));
+}
+
+Vehicle winner = race.StartRace();
+informerCLI.DisplayWinner(winner);
---
+++ RaceSimulator/Transportation/VehicleFactory.cs
@@ -0,0 +1,21 @@
+﻿using RaceSimulator.Transportation.Abstractions;
+
+namespace RaceSimulator.Transportation;
+
+public static class VehicleFactory
+{
+    public static AbstractVehicle CreateVehicle(string vehicleType)
+    {
+        // Initial factory implementation, requires additional thinking
+        return vehicleType switch
+        {
+            "Centaur" => new Centaur(),
+            "SevenLeagueBoots" => new SevenLeagueBoots(),
+            "MagicCarpet" => new MagicCarpet(),
+            "BabaYagasStupa" => new BabaYagasStupa(),
+
+            _ => throw new ArgumentException("Unknown vehicle type")
+        };
+    }
+}
+
