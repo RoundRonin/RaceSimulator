2d71d3c10d11434ea954fac6aca4b257b46095f1
Anton <roundronin8@gmail.com>
2024-10-29 21:05:51 +0300

    Added race simulation logic
    Race simulation logic is implemented with the idea of real-time
    simulation.
    
    It provides a loop that implemetns Tick-based simulation. 1 tick
    currently is 1 second. Each iteration of the loop the program updates
    states of all the objects based on their properties (implemented earlier
    within each of the vehicle classes)

---
+++ RaceSimulator/RaceLogic/RaceLogic.cs
@@ -0,0 +1,35 @@
+﻿using RaceSimulator.Transportation.Abstractions;
+using RaceSimulator.Presentation.Interfaces;
+
+namespace RaceSimulator.RaceLogic;
+
+public class RaceLogic(double distance, IPrinter printer)
+{
+    public double Distance { get; set; } = distance;
+    public IPrinter printer { get; set; } = printer;
+    public List<AbstractVehicle> Vehicles { get; set; } = new List<AbstractVehicle>();
+
+    public void RegisterVehicle(AbstractVehicle vehicle)
+    {
+        Vehicles.Add(vehicle);
+    }
+
+    public AbstractVehicle StartRace()
+    {
+        AbstractVehicle winner = null;
+        double currentPosition = 0;
+
+        while (true)
+        {
+            foreach (var vehicle in Vehicles)
+            {
+                currentPosition = vehicle.CalculatePosition();
+                printer.PrintFormattedLine(vehicle.Name, currentPosition.ToString());
+
+                if (currentPosition >= Distance) { return vehicle; }
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+}
